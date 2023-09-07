c2c61416df1f69dc3a8bcc5658a018bec59599fe
Sushmitha <68849782+sushmithaa20@users.noreply.github.com>
2023-09-08 00:22:19 +0530

    Update largest_product_in_series.py

--- largest_product_in_series.py
+++ largest_product_in_series.py
@@ -5,6 +5,31 @@ import sys
 
 def great_cons_prod(n,k,num):
     max_pro=0
+    while len(num) >= k:
+        prod=1
+        for i in range(k):
+            prod*=int(num[i])
+        max_pro = max(max_pro,prod)
+        num=str(num[1:])
+    return max_pro
+        
+
+t = int(input().strip())
+for a0 in range(t):
+    n,k = input().strip().split(' ')
+    n,k = [int(n),int(k)]
+    num = input().strip()
+    print(great_cons_prod(n,k,num))
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#!/bin/python3
+
+import sys
+
+
+def great_cons_prod(n,k,num):
+    max_pro=0
     for i in range(n-k+1):
         product=1
         for j in range(i,i+k):
